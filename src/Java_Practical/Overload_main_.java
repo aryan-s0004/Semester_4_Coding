bf00578ab76e72e04efe468f78917caa1361d654
Aryan Sahu <aryansahu0004@gmail.com>
2026-01-19 01:11:32 +0530

    Fixed  main method overloading

--- src/Java_Practical/Overload_main_.java
+++ src/Java_Practical/Overload_main_.java
@@ -2,6 +2,26 @@ package Java_Practical;
 
 public class Overload_main_ {
 
+    // Overloaded main method 2
+    public static void main(float[] arr2) {
+        System.out.println("\nOverloaded main 2 method called");
+
+        for (float i : arr2) {
+            System.out.print(i + " ");
+        }
+    }
+    /*
+    // Actual main method recognised by JVM why so ??
+    Solution :-
+public	JVM must access it from anywhere
+static	JVM does not create object
+void	JVM does not expect return value
+main	Fixed name known to JVM
+String[] args	To receive command line arguments
+
+                        */
+
+    // Actual main method recognised by JVM starts here
     public static void main(String[] args) {
 
         System.out.println("Main method started");
@@ -12,25 +32,21 @@ public class Overload_main_ {
         float[] data2 = {1.2f, 2.5f, 3.7f};
 
         // Step 2: Call overloaded main methods
-        main(data2);   // calling overloaded main (float[])
         main(data1);   // calling overloaded main (int[])
+        main(data2);   // calling overloaded main (float[])
     }
+    // Actual main method recognised by JVM ends here
 
     // Overloaded main method 1
     public static void main(int[] arr) {
         System.out.println("\nOverloaded main 1 method called");
 
-        for (int i : arr) {
-            System.out.print(i+" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
+
     }
 
-    // Overloaded main method 2
-    public static void main(float[] arr2) {
-        System.out.println("\nOverloaded main 2 method called");
-
-        for (float i : arr2) {
-            System.out.print(i+" ");
-        }
     }
-}
+

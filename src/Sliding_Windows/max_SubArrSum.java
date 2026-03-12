fca95b0ea9f42888fbf25bc60109b496bcd0a37a
Aryan Sahu <aryansahu0004@gmail.com>
2026-03-13 01:53:06 +0530

    feat(array): implement Kadane algorithm for maximum subarray sum

---
+++ src/Sliding_Windows/max_SubArrSum.java
@@ -0,0 +1,19 @@
+package Sliding_Windows;
+
+class MaxSubArrSum {
+
+    /*
+    Problem Description:
+    Find the maximum sum of a contiguous subarray.
+
+    Sample Input:
+    arr = [-2,1,-3,4,-1,2,1,-5,4]
+
+    Sample Output:
+    6
+    */
+
+    public static void main(String[] args) {
+
+    }
+}

d2ca06db0dabe06e28976929df3439bf6fbeab94
Aryan Sahu <aryansahu0004@gmail.com>
2026-03-13 01:53:04 +0530

    feat(sliding-window): add variable size sliding window template

---
+++ src/Sliding_Windows/var_size_Window_temp.java
@@ -0,0 +1,23 @@
+package Sliding_Windows;
+class VarSize_Window_Temp {
+
+    /*
+    Problem Description:
+    Template for solving variable size sliding window problems.
+
+    Used in problems like:
+    Longest substring without repeating characters
+    Longest substring with K distinct characters
+    Minimum window substring
+
+    Sample Input:
+    Example problem dependent.
+
+    Sample Output:
+    Example problem dependent.
+    */
+
+    public static void main(String[] args) {
+
+    }
+}

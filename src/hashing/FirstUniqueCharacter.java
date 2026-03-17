8dfb9af2e79ec099a2892d3212e8d3f95237f7df
Aryan Sahu <aryansahu0004@gmail.com>
2026-03-17 21:05:30 +0530

    feat: implement First Unique Character using frequency count

---
+++ src/hashing/FirstUniqueCharacter.java
@@ -0,0 +1,34 @@
+package hashing;/*
+ Problem: First Unique Character in a String
+
+Return index of first non-repeating character.
+
+ Input:
+s = "leetcode"
+
+ Output:
+0
+*/
+
+public class FirstUniqueCharacter {
+
+    public static int firstUniqChar(String s) {
+        int[] count = new int[26];
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("leetcode"));
+    }
+}

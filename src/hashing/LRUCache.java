d9f4841cf979c8b4fddfc7af65a7f5ab339948d0
Aryan Sahu <aryansahu0004@gmail.com>
2026-03-18 22:42:31 +0530

    feat: design LRU Cache using LinkedHashMap with access-order tracking

---
+++ src/hashing/LRUCache.java
@@ -0,0 +1,34 @@
+package hashing;
+
+import java.util.*;
+
+class LRUCache {
+    private LinkedHashMap<Integer, Integer> map;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new LinkedHashMap<>(capacity, 0.75f, true);
+    }
+
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+        if (map.size() > capacity) {
+            int oldest = map.keySet().iterator().next();
+            map.remove(oldest);
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+        cache.put(1,1);
+        cache.put(2,2);
+        System.out.println(cache.get(1));
+        cache.put(3,3);
+        System.out.println(cache.get(2));
+    }
+}
